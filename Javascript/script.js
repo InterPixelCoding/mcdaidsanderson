8ebc534a6d6695a255b77c1d3c9362279cdd2568
InterPixel <theweeklyjubs@gmail.com>
2023-02-17 17:47:41 +0000

    Started on Listen Page

--- Javascript/script.js
+++ Javascript/script.js
@@ -22,10 +22,48 @@ header.style.height =`${headerHeight}px`
 
 const menuButton = document.querySelector('.menu-button');
 const menu = document.querySelector('header > nav')
-menuButton.addEventListener("click", function () {
+menuButton.addEventListener("click", () => {
 	menu.classList.toggle('menu-active');
 	if(menuButton.innerHTML === `<i class="fa-solid fa-bars"></i>`) {
 		header.style.height = `100%`
 		menuButton.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
 	} else {menuButton.innerHTML = `<i class="fa-solid fa-bars"></i>`; header.style.height = `${headerHeight}px`}
 })
+
+// Listen Page
+audioItems = [
+'American Reels',
+'Glasgow Girl',
+'Lancashire Hills',
+"There's a welcome",
+'Until next summer'
+]
+
+const listenContainer = document.querySelector('.audio-widget-container');
+
+audioItems.forEach(track => {
+	trackFormatted = (track.replace(` `, `-`).replace(` `, `-`)).toLowerCase();
+	newTrackWidget = document.createElement('div');
+	newTrackWidget.classList.add('track-container')
+
+	trackTitle = document.createElement("h3");
+	trackTitle.textContent = track;
+
+	trackButton = document.createElement("button");
+	trackButton.innerHTML = `<i class="fa-solid fa-play"></i>`
+	trackTimeline = document.createElement("div");
+
+	trackAudio = document.createElement("audio");
+	trackAudio.src = `../Audio-Files/${trackFormatted}.mp3`
+
+	trackButton.addEventListener("click", () => {
+		trackAudio.play()
+	})
+
+	newTrackWidget.appendChild(trackAudio)
+	newTrackWidget.appendChild(trackTitle);
+	newTrackWidget.appendChild(trackButton);
+	newTrackWidget.appendChild(trackTimeline);
+
+	listenContainer.appendChild(newTrackWidget);
+});
