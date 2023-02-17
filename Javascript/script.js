34ed5bcbb272bcc22abfc1cc2f2a8fd227f4e578
InterPixel <theweeklyjubs@gmail.com>
2023-02-17 13:56:13 +0000

    Added Home Page

--- Javascript/script.js
+++ Javascript/script.js
@@ -15,3 +15,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
 	hiddenElements.forEach((el) => observer.observe(el));
 
 });
+// height: auto hack with javascript
+const header = document.querySelector('header')
+var headerHeight = header.offsetHeight;
+header.style.height =`${headerHeight}px`
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('header > nav')
+menuButton.addEventListener("click", function () {
+	menu.classList.toggle('menu-active');
+	if(menuButton.innerHTML === `<i class="fa-solid fa-bars"></i>`) {
+		header.style.height = `100%`
+		menuButton.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+	} else {menuButton.innerHTML = `<i class="fa-solid fa-bars"></i>`; header.style.height = `${headerHeight}px`}
+})
