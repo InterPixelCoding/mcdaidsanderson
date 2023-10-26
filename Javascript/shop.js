4ca4b3b8ff3a36cca83b270f63257f9014328b55
InterPixel <theweeklyjubs@gmail.com>
2023-10-26 11:30:54 +0000

    Paypal Working but no styling
    Using the github environment to manage iframe request security

--- Javascript/shop.js
+++ Javascript/shop.js
@@ -52,6 +52,39 @@ function addClass(className, objectArray) {
     });
 }
 
+function getElements(masterContainer) {
+    // Define the selectors for the iframe-contained elements
+    const selectors = ['.card-field-name', '.card-field-number', '.card-field-expiry', '.card-field-cv'];
+  
+    // Create an object to store the elements
+    const elements = {};
+  
+    // Find and access the iframes within the masterContainer
+    const iframes = document.querySelectorAll('#checkout-form > div > div > iframe');
+  
+    iframes.forEach((iframe, index) => {
+      // Access the iframe's content document
+      const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+  
+      // Find the elements using the provided selectors and store them in the elements object
+      selectors.forEach(selector => {
+        const element = iframeDocument.querySelector(selector);
+        if (element) {
+          elements[selector] = element;
+        }
+      });
+    });
+  
+    return elements;
+}
+  
+  // Example usage:
+  const masterContainer = document.querySelector('#checkout-form');
+  const styleElements = getElements(masterContainer);
+  
+  console.log(styleElements);
+  
+
 // Generate Shop Items
 CDObjectArray.forEach(function(cd, index){
         const newShopItem = document.createElement("div");
@@ -71,6 +104,13 @@ CDObjectArray.forEach(function(cd, index){
                         const price = document.createElement("span");
                     const checkout = document.querySelector('.checkout')
                         buyNowButton.addEventListener("click", () => {
+                            // Style Credit Card Elements
+                            setTimeout(() => {
+                                console.log('timeout finished')
+                                const styleElements = document.querySelector('#checkout-form')
+                                console.log(styleElements)
+                            }, 5000);
+
                             const checkoutImage = document.querySelector('.checkout > .basket > .cd-cover');
                             const checkoutPrice = document.querySelector('.total-price')
                             const checkoutShopItem = document.querySelector('.name-of-product')
@@ -163,3 +203,96 @@ exit.addEventListener("click", () => {
     addClass('disabled', [checkout, popUp])
 })
 
+// Render the button component
+paypal
+  .Buttons({
+    // Sets up the transaction when a payment button is clicked
+    createOrder: function (data) {
+      return fetch("myserver.com/api/orders", {
+        method: "POST",
+        // Use the "body" parameter to optionally pass additional order information
+        // such as product ID or amount
+        body: {
+          paymentSource: data.paymentSource
+        }
+      })
+        .then((response) => response.json())
+        .then((order) => order.id);
+    },
+    // Finalize the transaction after payer approval
+    onApprove: function (data) {
+      return fetch(`myserver.com/api/orders/${data.orderID}/capture`, {
+        method: "POST",
+      })
+        .then((response) => response.json())
+        .then((orderData) => {
+          // Successful capture! For dev/demo purposes:
+          console.log("Capture result", orderData, JSON.stringify(orderData, null, 2));
+          var transaction = orderData.purchase_units[0].payments.captures[0];
+          // Show a success message within this page. For example:
+          // var element = document.getElementById('paypal-button-container');
+          // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+          // Or go to another URL: actions.redirect('thank_you.html');
+        });
+    },
+    onError: function (error) {
+        // Do something with the error from the SDK
+    }
+  })
+  .render("#paypal-button-container");
+
+const cardField = paypal.CardFields({
+    createOrder: function (data) {
+        return fetch("myserver.com/api/orders", {
+            method: "post",
+            body: {
+                paymentSource: data.paymentSource
+            }
+        })
+        .then((res) => {
+            return res.json();
+        })
+        .then((orderData) => {
+            return orderData.id;
+        });
+    },
+    onApprove: function (data) {
+        const { orderID } = data;
+        return fetch(`myserver.com/api/orders/${orderID}/capture`, {
+            method: "post",
+        })
+        .then((res) => {
+            return res.json();
+        })
+        .then((orderData) => {
+            // Redirect to success page
+        });
+    },
+    onError: function (error) {
+        // Do something with the error from the SDK
+    },
+});
+
+// Render each field after checking for eligibility
+if (cardField.isEligible()) {
+    const nameField = cardField.NameField();
+    nameField.render('#card-name-field-container');
+
+    const numberField = cardField.NumberField();
+    numberField.render('#card-number-field-container');
+
+    const cvvField = cardField.CVVField();
+    cvvField.render('#card-cvv-field-container');
+
+    const expiryField = cardField.ExpiryField();
+    expiryField.render('#card-expiry-field-container');
+
+    // Add click listener to submit button and call the submit function on the CardField component
+    document.getElementById("card-field-submit-button").addEventListener("click", () => {
+        cardField
+        .submit()
+        .then(() => {
+            // submit successful
+        });
+    });
+};
